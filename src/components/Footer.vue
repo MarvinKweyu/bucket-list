cf68d164cbbcf688903ca8253b5b8b1b7c83261c
MarvinKweyu <mkweyu1@gmail.com>
2021-06-10 11:13:09 +0300

    chore: add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+,<template>
+  <v-container class="bottom">
+    <v-row class="d-flex justify-center">
+      <p>
+        <a
+          href="https://marvinkweyu.net"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Marvin Kweyu</a
+        >
+      </p>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+.bottom {
+  bottom: 0;
+  position: absolute;
+}
+</style>
