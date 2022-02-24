5babd579c3736aacb39dcad1527fc75452297c5b
Marvin Kweyu <mkweyu1@gmail.com>
2022-02-24 16:47:40 +0300

    chore: add new item to list

--- src/components/BoardGroup.vue
+++ src/components/BoardGroup.vue
@@ -5,10 +5,46 @@
       <p class="item-count">{{ categoryData.length }}</p>
     </div>
     <div class="add-btn d-flex justify-center" v-if="title === 'Todo'">
-      <v-icon color="teal" style="background-color: #e8f1f0">mdi-plus</v-icon>
+      <v-icon
+        color="teal"
+        style="background-color: #e8f1f0"
+        @click="newItem = !newItem"
+        >mdi-plus</v-icon
+      >
     </div>
 
     <div class="item-list">
+      <div v-if="newItem" class="mt-2">
+        <v-text-field
+          label="Item title"
+          color="#8c8c8c"
+          class="mx-2"
+          v-model="newItemData.title"
+        ></v-text-field>
+        <v-textarea
+          v-model="newItemData.description"
+          name="input-7-1"
+          hint="Add new item"
+          outlined
+          color="#8c8c8c"
+          label="Item description"
+          class="mx-2"
+          auto-grow
+        ></v-textarea>
+        <div class="d-flex justify-space-between mx-4">
+          <v-btn
+            @click="newItem = false"
+            color="secondary"
+            elevation="2"
+            outlined
+            plain
+            >Cancel</v-btn
+          >
+          <v-btn color="secondary" elevation="2" outlined plain @click="addItem"
+            >Add</v-btn
+          >
+        </div>
+      </div>
       <ItemDetail
         class="item-detail mt-2"
         v-for="item in categoryData"
@@ -37,7 +73,14 @@ export default {
     }
   },
   data() {
-    return {}
+    return {
+      newItem: false,
+      newItemData: {
+        title: '',
+        description: '',
+        category: 'toDo'
+      }
+    }
   },
   components: {
     ItemDetail
@@ -48,6 +91,15 @@ export default {
     },
     getItemsOfCategory() {
       return this.allOfThem.filter((item) => item.category === this.category)
+    },
+    addItem() {
+      this.$store.dispatch('createProject', this.newItemData)
+      this.newItem = false
+      this.newItemData = {
+        title: '',
+        description: '',
+        category: 'toDo'
+      }
     }
   }
 }
@@ -61,6 +113,12 @@ export default {
 .board-group {
   border-radius: 10px;
   height: 86vh;
+}
+
+@media (max-width: 800px) {
+  .board-group {
+    height: 30vh;
+  }
 }
 
 .item-list {
